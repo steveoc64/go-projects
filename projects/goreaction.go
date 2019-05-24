d8410454cdd9a87217401e98374af2ef9c1fb77a
Jacob <Jacalz@users.noreply.github.com>
2019-05-24 08:14:15 +0200

    Better commenting in GoReaction

--- projects/goreaction.go
+++ projects/goreaction.go
@@ -10,15 +10,19 @@ func main() {
 	fmt.Print("Press enter to start!")
 	fmt.Scanln()
 
+	// Sleep the main thread for a random number of seconds between 1 and 15
 	time.Sleep(time.Duration(rand.Int63n(15)) * time.Second)
 
+	// Take the time now and print a box
 	start := time.Now()
 	row := "<------------->"
 	fmt.Printf("			%s\n			%s\n			%s\n			%s\n", row, row, row, row)
 
+	// When user presses enter, a new time is taken and then we calculate the time between those points.
 	fmt.Scanln()
 	end := time.Now()
 	elapsed := end.Sub(start) / 1000000 //Convert nanoseconds to milliseconds
 
 	fmt.Printf("Your reaction time is %d milliseconds!\n", elapsed)
+
 }
