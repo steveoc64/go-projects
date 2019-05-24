7f508ca67dbb461d12f4a3c2d5cf7541ab1031f7
Jacob <Jacalz@users.noreply.github.com>
2019-05-24 09:32:39 +0200

    Add the file for the straight line equation calculator

---
+++ projects/equation-straightline.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// Calculates the values of equation from points on the line
+func LineEquation(Point1X, Point1Y, Point2X, Point2Y float64) (Gradient, Intercept float64) {
+	Gradient = (Point2Y - Point1Y) / (Point2X - Point1X)
+	Intercept = Point1Y - (Gradient * Point1X)
+	return Gradient, Intercept
+}
+
+func main() {
+	var Point1X, Point1Y, Point2X, Point2Y float64
+	fmt.Println("Enter two points from straight line graph to get the equation for it!")
+
+	// Enter the first point from the line
+	fmt.Print("\nEnter first point (x, y): ")
+	fmt.Scanf("(%v, %v)", &Point1X, &Point1Y)
+
+	// Enter the second point on the line
+	fmt.Print("Enter second point (x, y): ")
+	fmt.Scanf("(%v, %v)", &Point2X, &Point2Y)
+
+	// Output values from function to respectiv variables
+	Gradient, Intercept := LineEquation(Point1X, Point1Y, Point2X, Point2Y)
+
+	// Get it to not print a + before a negative number
+	fmt.Println("\nYou equation is:")
+	if Intercept > 0 {
+		fmt.Printf("y = %vx + %v", Gradient, Intercept)
+	} else if Intercept < 0 {
+		Intercept = -Intercept
+		fmt.Printf("y = %vx - %v", Gradient, Intercept)
+	}
+
+}
