37bfbe3cc8bd33346dfee09a7b93a6cd0bb30677
Jacob <Jacalz@users.noreply.github.com>
2019-05-28 20:43:56 +0200

    Add merit point calculator for gymnasiet in Sweden

---
+++ projects/meritcalc.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func betygsPoäng(bokstav string) (poäng float64) {
+
+	switch bokstav {
+	case "A":
+		poäng = 20
+	case "B":
+		poäng = 17.5
+	case "C":
+		poäng = 15
+	case "D":
+		poäng = 12.5
+	case "E":
+		poäng = 10
+	default:
+		fmt.Println("Inte ett giltigt betyg!")
+		os.Exit(2)
+	}
+
+	return poäng
+}
+
+func merit(betyg int, extra float64) (merit float64) {
+	fmt.Println("Skriv in ett betyg per rad:")
+
+	var summa float64
+	var bokstav string
+
+	for i := 1; i <= betyg; i++ {
+		fmt.Scanln(&bokstav)
+		summa = summa + betygsPoäng(bokstav)
+	}
+
+	merit = (summa / float64(betyg)) + extra
+
+	return merit
+}
+
+func main() {
+	var betyg int
+	var extra float64
+	fmt.Print("Ange antal betyg från gymnasiet: ")
+	fmt.Scanln(&betyg)
+
+	fmt.Print("Antal extramerit från kurser: ")
+	fmt.Scanln(&extra)
+
+	if extra < 0 || extra > 2.5 {
+		fmt.Println("Inte ett giltigt antal extramerit!")
+		os.Exit(2)
+	}
+
+	fmt.Printf("Meritpoäng: %.2f", merit(betyg, extra))
+}
