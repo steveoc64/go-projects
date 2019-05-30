7ed86006c6f6060cdd7751140b4734c234c0b20a
Jacob <Jacalz@users.noreply.github.com>
2019-05-30 10:13:53 +0200

    Fix var name in function and input changes
    Function threadMass had the input variable named "threadMass" instead of "threadDiameter", made the string input go to variable input along with copying material properties to variable material and also better error handling.

--- projects/improved-gruvlinan.go
+++ projects/improved-gruvlinan.go
@@ -19,8 +19,8 @@ func threadArea(ThreadDiameter float64) float64 {
 }
 
 // Beräkna trådens vikt utifrån matematiska formeln för vikt.
-func threadMass(ThreadMass, Dencity float64) float64 {
-	return (threadArea(ThreadMass) / 1000000) * Dencity * (Length / 1000)
+func threadMass(ThreadDiameter, Dencity float64) float64 {
+	return (threadArea(ThreadDiameter) / 1000000) * Dencity * (Length / 1000)
 }
 
 // Beräkning av trådens tillåtna spänning.
@@ -104,43 +104,42 @@ type Material struct {
 
 func main() {
 
-	var material string
-
 	// Alla egenskaper för materialen utifrån Material-typen.
 	titan := Material{105000, 4600, 747.5, 962.5, 2.5, 500}
 	cfrp := Material{107000, 1550, 800, 800, 2.5, 500}
 	nylon := Material{2910, 1130, 72.4, 127.5, 5, 2000}
 
+	var input string
 	fmt.Print("\nMöjliga material är titan, cfrp eller nylon.\nVälj material att använda: ")
-	fmt.Scanln(&material)
+	fmt.Scanln(&input)
 
-	// Skapa ett val av material där materialets egenskaper kopieras till choice.
-	var choice *Material
-	switch material {
+	// Skapa ett val av material där materialets egenskaper kopieras till material.
+	var material *Material
+	switch input {
 	case "titan":
-		choice = &titan
+		material = &titan
 	case "cfrp":
-		choice = &cfrp
+		material = &cfrp
 	case "nylon":
-		choice = &nylon
+		material = &nylon
 	default:
-		fmt.Println("Error: Inte ett giltigt material!")
+		fmt.Printf("Error: %s är inte ett giltigt material!\n", input)
 		return
 	}
 
 	// Beräkning av tillåten spänning samt definiering av trådens area och vikt.
-	σTill := choice.YieldStrength / SafetyFactor
-	ThreadArea := threadArea(choice.ThreadDiameter)
-	ThreadMass := threadMass(choice.ThreadDiameter, choice.Dencity)
+	σTill := material.YieldStrength / SafetyFactor
+	ThreadArea := threadArea(material.ThreadDiameter)
+	ThreadMass := threadMass(material.ThreadDiameter, material.Dencity)
 
 	// Skriv ut säkerhetsfaktorn och tråddiametern.
 	fmt.Println("\nSäkerhetsfaktor:", SafetyFactor)
-	fmt.Println("Diameter på lös tråd:", choice.ThreadDiameter, "mm")
+	fmt.Println("Diameter på lös tråd:", material.ThreadDiameter, "mm")
 
 	// Definiering av variabler för tillåten styrka, sträckgräns och brottgräns utifrån funktionerna.
 	AllowedThreadStrength := allowedThreadStrenght(σTill, ThreadArea, ThreadMass)
-	ThreadYieldStength := threadYieldStength(choice.YieldStrength, ThreadArea, ThreadMass)
-	ThreadTensileStrength := threadTensileStrength(choice.TensileStrength, ThreadArea, ThreadMass)
+	ThreadYieldStength := threadYieldStength(material.YieldStrength, ThreadArea, ThreadMass)
+	ThreadTensileStrength := threadTensileStrength(material.TensileStrength, ThreadArea, ThreadMass)
 
 	// Definiering av variabel för antalet trådar utifrån funktionen.
 	RequiredThreads := requiredThreads(AllowedThreadStrength)
@@ -168,7 +167,7 @@ func main() {
 	fmt.Printf("Teoretisk brottgräns: %.2f kg\n", WireStrengthTensile)
 
 	// Definiera totala förlängningen och skriv ut den.
-	TotalExtension := totalExtension(WireMass, WireArea, choice.Elasticity)
+	TotalExtension := totalExtension(WireMass, WireArea, material.Elasticity)
 	fmt.Printf("\nTotal förlängning: %.2f mm\n", TotalExtension)
 
 	// Beräkna procentuella förlängningen.
@@ -176,9 +175,9 @@ func main() {
 	fmt.Printf("Procentuell förlängning: %.5f%%\n", Epsilon)
 
 	// Definiering av längen av trumman vid ett varv och tre varv samt utskrift av svaren, inklusive trummans diameter.
-	SpoolLength := spoolLength(choice.SpoolDiameter, WireDiameter, TotalExtension)
-	ThreeLapsSpoolLength := threeLapsSpoolLength(choice.SpoolDiameter, WireDiameter, TotalExtension)
-	fmt.Printf("\nDiameter på trumman: %.2f mm\n", choice.SpoolDiameter)
+	SpoolLength := spoolLength(material.SpoolDiameter, WireDiameter, TotalExtension)
+	ThreeLapsSpoolLength := threeLapsSpoolLength(material.SpoolDiameter, WireDiameter, TotalExtension)
+	fmt.Printf("\nDiameter på trumman: %.2f mm\n", material.SpoolDiameter)
 	fmt.Printf("Längd på trumman (ett lager): %.2f mm\n", SpoolLength)
 	fmt.Printf("Längd på trumman (tre lager): %.2f mm\n", ThreeLapsSpoolLength)
 
