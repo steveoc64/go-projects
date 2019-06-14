8d9a1891b7adf48d2bd281ea22003c68aa738b33
Jacob <Jacalz@users.noreply.github.com>
2019-06-14 12:47:40 +0200

    Update gomake with cleanup of crosscompiles and possibility to pass both release and cross-compile

--- gomake/gomake.go
+++ gomake/gomake.go
@@ -20,27 +20,40 @@ func compileRelease64() {
 }
 
 // crossCompileRelease64 compiles the binary for linux, macOS and windows and all without debug symbols.
-func crossCompile64() {
-	fmt.Println("Compiling 64-bit binaries (no debug) for Linux, MacOS and Windows...")
+func crossCompile64(Release bool) {
+
+	// Build as debug if release isn't passed as flag and if it is we will build without debug symbols
+	var LinuxCommand, DarwinCommand, WindowsCommand string
+	if Release {
+		fmt.Println("Compiling 64-bit binaries (no debug) for Linux, MacOS and Windows...")
+		LinuxCommand = `go build -ldflags="-s -w" -o $(pwd)/main-linux-x64`
+		DarwinCommand = `go build -ldflags="-s -w" -o $(pwd)/main-darwin-x64`
+		WindowsCommand = `go build -ldflags="-s -w" -o $(pwd)/main-windows-x64`
+	} else {
+		fmt.Println("Compiling 64-bit binaries for Linux, MacOS and Windows...")
+		LinuxCommand = "go build -o $(pwd)/main-linux-x64"
+		DarwinCommand = "go build -o $(pwd)/main-darwin-x64"
+		WindowsCommand = "go build -o $(pwd)/main-windows-x64"
+	}
 
 	// cmdOne has the command to build for linux, cmdOne.Env has the environment variables and then we run with cmdOne.Run.
-	cmdOne := exec.Command("/bin/bash", "-c", `go build -ldflags="-s -w" -o $(pwd)/main-linux-x64`)
-	cmdOne.Env = append(os.Environ(), "GOOS=linux", "GOARCH=amd64")
-	if err := cmdOne.Run(); err != nil {
+	linux := exec.Command("/bin/bash", "-c", LinuxCommand)
+	linux.Env = append(os.Environ(), "GOOS=linux", "GOARCH=amd64")
+	if err := linux.Run(); err != nil {
 		log.Fatal(err)
 	}
 
 	// cmdTwo has the command to build for macOS, cmdTwo.Env has the environment variables and then we run with cmdTwo.Run.
-	cmdTwo := exec.Command("/bin/bash", "-c", `go build -ldflags="-s -w" -o $(pwd)/main-darwin-x64`)
-	cmdTwo.Env = append(os.Environ(), "GOOS=darwin", "GOARCH=amd64")
-	if err := cmdTwo.Run(); err != nil {
+	darwin := exec.Command("/bin/bash", "-c", DarwinCommand)
+	darwin.Env = append(os.Environ(), "GOOS=darwin", "GOARCH=amd64")
+	if err := darwin.Run(); err != nil {
 		log.Fatal(err)
 	}
 
 	// cmdThree has the command to build for windows, cmdThree.Env has the environment variables and then we run with cmdThree.Run.
-	cmdThree := exec.Command("/bin/bash", "-c", `go build -ldflags="-s -w" -o $(pwd)/main-windows-x64`)
-	cmdThree.Env = append(os.Environ(), "GOOS=windows", "GOARCH=amd64")
-	if err := cmdThree.Run(); err != nil {
+	windows := exec.Command("/bin/bash", "-c", WindowsCommand)
+	windows.Env = append(os.Environ(), "GOOS=windows", "GOARCH=amd64")
+	if err := windows.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -48,9 +61,17 @@ func crossCompile64() {
 // Just a standard compilation using go build.
 func compile() {
 	fmt.Println("Running standard compile...")
-	cmd := exec.Command("go", "build")
-	if err := cmd.Run(); err != nil {
+	compile := exec.Command("go", "build")
+	if err := compile.Run(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func cleanupCrossCompiles() {
+	fmt.Println("Cleaning up files from cross compiling...")
+	remove := exec.Command("/bin/bash", "-c", "rm main-linux-x64 main-darwin-x64 main-windows-x64")
+	if err := remove.Run(); err != nil {
+		log.Fatal(err, "\nNo cross compiled packages to remove")
 	}
 }
 
@@ -65,13 +86,19 @@ func main() {
 	var Release *bool = flag.Bool("release", false, "strip debug data for a smaller binary file.")
 	var CrossCompile *bool = flag.Bool("cross-compile", false, "compiles the package for Amd64 on Linux, MacOS and Windows without debug symbols.")
 	flag.Parse()
+	var Clean string = flag.Arg(0)
 
-	// Handle values for the command line flags passed by users.
-	if *Release {
-		compileRelease64()
-	} else if *CrossCompile {
-		crossCompile64()
-	} else {
-		compile()
+	if Clean == "clean" {
+		cleanupCrossCompiles()
+	} else if *Release || *CrossCompile {
+
+		// Handle values for the command line flags passed by users.
+		if *Release && *CrossCompile == false {
+			compileRelease64()
+		} else if *CrossCompile {
+			crossCompile64(*Release)
+		} else {
+			compile()
+		}
 	}
 }
