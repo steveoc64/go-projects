a3419fe230e1324653c137fbc207fb8fe2d11e48
Jacob <Jacalz@users.noreply.github.com>
2019-06-15 14:47:30 +0200

    Add comments and clean up file

--- projects/solus-packaging/solfetch.go
+++ projects/solus-packaging/solfetch.go
@@ -7,17 +7,19 @@ import (
 )
 
 func main() {
+	// Let flag parse the command line arguments and then input the first argument in to repo variable
 	flag.Parse()
-
 	var repo string = flag.Arg(0)
+	
+	// Handle empty arguments by printing usage
 	if repo == "" {
 		log.Fatalln("Usage: solfetch [repository name]")
 	}
 
-	path := "https://dev.getsol.us/source/" + repo + ".git"
-	command := "git clone " + path
+	path := "git clone https://dev.getsol.us/source/" + repo + ".git"
 
-	cmd := exec.Command("/bin/bash", "-c", command)
+	// Run the git command to download the whole repository
+	cmd := exec.Command("/bin/bash", "-c", path)
 	if err := cmd.Run(); err != nil {
 		log.Fatal(err, "\nSpecified repository doesn't exist or your internet connection is down")
 	}
