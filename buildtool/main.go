1358e1ad124bbdbef644eb14c94efe0b172434cd
Jacob <Jacalz@users.noreply.github.com>
2019-07-12 15:43:25 +0200

    Update to get language from file ending

--- buildtool/main.go
+++ buildtool/main.go
@@ -11,51 +11,48 @@ import (
 const compileflags = "-O3 -march=native -mtune=native -flto -ffast-math -pipe -Wall -v"
 
 func runCompile(command string) {
-    compile := exec.Command("sh", "-c", command)
-    output, err := compile.CombinedOutput()
-    if err != nil {
-        log.Fatal(output, err)
-    } else {
-	print(output)
-    }
+	compile := exec.Command("sh", "-c", command)
+	output, err := compile.CombinedOutput()
+	if err != nil {
+		log.Fatal(output, err)
+	} else {
+		print(output)
+	}
 }
 
 func print(output []byte) {
-    fmt.Printf("%s\n", output)
+	fmt.Printf("%s\n", output)
 }
 
 func compileCPP(file string) {
-    command := "clang++ -std=c++14 -stdlib=libc++ " + file + " -o compiled-cpp " + compileflags
-    runCompile(command)
+	command := "clang++ -std=c++14 -stdlib=libc++ " + file + " -o compiled-cpp " + compileflags
+	runCompile(command)
 }
 
-func compileGO(file string) {
-    command := `go build -ldflags="-s -w" -o compiled-go ` + file
-    runCompile(command)
+func compileGo(file string) {
+	command := `go build -ldflags="-s -w" -o compiled-go ` + file
+	runCompile(command)
 }
 
-func checkLang(language, file string) {
-    switch language {
-    case "c++", "C++", "cpp":
-        compileCPP(file)
-    case "go", "Go", "GO":
-        compileGO(file)
-    case "c", "C":
-        compileC(file)
-    default:
-	fmt.Println("Usage:\n   build [language] [file-to-build]\n",
-            "     Valid languages:\n",
-            "       Go  (Builds without debug and dwarf data)\n",
-            "       C   (Uses perforamce optimized compiler flags with Clang compiler)\n",
-            "       C++ (Uses same optimizing compiler flags as C in Clang++ compiler)\n",
-            "\n       File to build:\n",
-            "       Specifies the file to build.",
-        )
-    }
+func checkLang(file string) {
+	if path.Match("*.cpp", file) || path.Match("*.cxx", file) {
+		compileCPP(file)
+	} else if path.Match("*.go", file) {
+		compileGo(file)
+	} else {
+		fmt.Println("Usage:\n   build [file-to-build]\n",
+			"     Valid languages:\n",
+			"       Go  (Builds without debug and dwarf data)\n",
+			"       C++ (Uses same optimizing compiler flags as C in Clang++ compiler)\n",
+			"\n       File to build:\n",
+			"       Specifies the file to build and decides upon language from file ending.",
+		)
+	}
+
 }
 
 func main() {
-    flag.Parse()
-    language, file := flag.Arg(0), flag.Arg(1)
-    checkLang(language, file)
+	flag.Parse()
+	file := flag.Arg(0)
+	checkLang(file)
 }
