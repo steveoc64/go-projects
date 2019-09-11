5020e3b14d2942ac13f4d83424b285d0e942e8e1
Jacob <Jacalz@users.noreply.github.com>
2019-09-11 15:53:27 +0000

    Add some comment to stop go vet complaining

--- school/passagehistorik/xml.go
+++ school/passagehistorik/xml.go
@@ -2,13 +2,15 @@ package main
 
 import "encoding/xml"
 
+// Data has the xml data for the initial data tag.
 type Data struct {
-	XMLName   xml.Name   `xml:"data"`
-	Person    []Person   `xml:"person"`
+	XMLName xml.Name `xml:"data"`
+	Person  []Person `xml:"person"`
 }
 
+// Person keeps track of the data per person inside the data tag.
 type Person struct {
-	XMLName    xml.Name   `xml:"person"`
-	Name       string     `xml:"name"`
-	Visits     int        `xml:"visits"`
+	XMLName xml.Name `xml:"person"`
+	Name    string   `xml:"name"`
+	Visits  int      `xml:"visits"`
 }
