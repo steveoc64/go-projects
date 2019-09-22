c14f3b14a1d36ac0c21316351ab69e125f12d411
Jacob <Jacalz@users.noreply.github.com>
2019-09-22 08:55:09 +0200

    Add more xml stuff in to where they belong

--- school/cmpp/xml.go
+++ school/cmpp/xml.go
@@ -1,6 +1,11 @@
 package main
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 // Data has the xml data for the initial data tag.
 type Data struct {
@@ -12,4 +17,26 @@ type Data struct {
 type Person struct {
 	Name   string `xml:"name"`
 	Visits int    `xml:"visits"`
+}
+
+// ReadDataFromXML reads data from an xml file, couldn't be simpler.
+func ReadDataFromXML() Data {
+
+	// Open up the xml file that already exists.
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Make sure to close it also.
+	defer file.Close()
+
+	// Read the data from the opened file.
+	byteValue, _ := ioutil.ReadAll(file)
+
+	// Unmarshal the xml data in to our Data struct.
+	data := Data{}
+	xml.Unmarshal(byteValue, &data)
+
+	return data
 }
