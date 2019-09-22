7f660b2c6dbf4b6746f857a5a0f7667621236296
Jacob <Jacalz@users.noreply.github.com>
2019-09-22 08:53:38 +0200

    Create pdf.go

---
+++ school/cmpp/pdf.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+
+	"github.com/ledongthuc/pdf"
+)
+
+// ReadPDF reads the content of the whole pdf file and prints it as text.
+func ReadPDF(path string) (string, error) {
+
+	// Open the pdf file using the pdf library.
+	file, result, err := pdf.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	// remember to close file.
+	defer file.Close()
+
+	// Create new buffer.
+	var buf bytes.Buffer
+
+	// Make sure to get plain text from pdf.
+	buffer, err := result.GetPlainText()
+	if err != nil {
+		return "", err
+	}
+
+	// Read the buffer that we took plaintext from.
+	buf.ReadFrom(buffer)
+
+	return buf.String(), nil
+}
