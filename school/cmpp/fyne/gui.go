1707ab51eea43d3e1827e0e487342e9a7f701c4a
Jacob <Jacalz@users.noreply.github.com>
2019-09-24 13:59:38 +0000

    Make the whole applications scrollable

--- school/cmpp/fyne/gui.go
+++ school/cmpp/fyne/gui.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 
 	"fyne.io/fyne"
@@ -17,8 +18,14 @@ func InitGui() {
 	// Create the window for our user interface.
 	window := app.NewWindow("CNG Medley PDF Parser")
 
+	// Make a vertical box to house all our widgets in.
+	box := widget.NewVBox()
+
 	// Fyne doesn't support file chooser yet so we need to enter the filename manually. Should not be a problem for files in the same folder though.
 	inputedFile := widget.NewEntry()
+
+	// Add inputedFile to the box.
+	box.Append(inputedFile)
 
 	// Create out number input entry.
 	inputedNumber := widget.NewEntry()
@@ -30,11 +37,6 @@ func InitGui() {
 	// Just a little spacer as a quick workaround getting a gap.
 	spacer := layout.NewSpacer()
 
-	/*
-		scrollable := widget.NewScrollContainer(dataLabel)
-		scrollable.Resize(fyne.NewSize(400, 600))
-	*/
-
 	// Create the import button for our file.
 	importPDF := widget.NewButton("Importera data från pdf ovan", func() {
 		// Load the imported data from the inputed pdf.
@@ -44,25 +46,42 @@ func InitGui() {
 		dataLabel.SetText("Antal elever under den veckan: " + strconv.Itoa(visitors))
 	})
 
+	// Add three more widgets to the box.
+	box.Append(importPDF)
+	box.Append(spacer)
+	box.Append(inputedNumber)
+
 	// Create the button for showing visitors in the gui.
 	displayLessThan := widget.NewButton("Visa antal elever med färre besök än valt nummer ovan", func() {
-		// Get the text data that is inputed in the entry above this button.
-		text := inputedNumber.Text
+		// Resize the window to something that's useable.
+		window.Resize(fyne.NewSize(400, 550))
 
-		// Run the name printer for strings and add the data to teh label.
-		names := StringLessThan(CheckNumber(text))
-		dataLabel.SetText(names)
+		// Run the name printer for strings and add the data to the label.
+		lessthan := CheckNumber(inputedNumber.Text)
+
+		// Loop through names and add a new label for each name.
+		data := ReadDataFromXML()
+		for i := 0; i < len(data.Person); i++ {
+			if data.Person[i].Visits < lessthan {
+				box.Append(widget.NewLabel(fmt.Sprintf("%s: %v besök", data.Person[i].Name, data.Person[i].Visits)))
+			}
+		}
+
+		// Resize the window a little bit to avoid overlapping data.
+		window.Resize(fyne.NewSize(400, 600))
 	})
 
+	// Add the last button to the box.
+	box.Append(displayLessThan)
+
+	// Make the whole box scrollable.
+	scroll := widget.NewScrollContainer(box)
+
 	// Set our content in the application.
-	window.SetContent(widget.NewVBox(
-		inputedFile,
-		importPDF,
-		spacer,
-		inputedNumber,
-		displayLessThan,
-		dataLabel,
-	))
+	window.SetContent(fyne.NewContainerWithLayout(layout.NewBorderLayout(nil, nil, nil, nil), scroll))
+
+	// Set a default size for the window.
+	window.Resize(fyne.NewSize(400, 170))
 
 	// Show all our widgets and initialize out main gui loop.
 	window.ShowAndRun()
