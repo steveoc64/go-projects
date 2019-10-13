04a3e56d15b800fb049b6136e6d5e567883e728f
Jacob <Jacalz@users.noreply.github.com>
2019-10-13 11:54:13 +0200

    Refractor and move solfetch

---
+++ projects/solus-packaging/solfetch/solfetch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+// The fetch command runs the git clone command and prints the output to terminal.
+func fetch(command string) {
+	fetch := exec.Command("sh", "-c", command)
+	output, err := fetch.CombinedOutput()
+	if err != nil {
+		log.Fatalf("%s", output)
+	} else {
+		fmt.Printf("%s", output)
+	}
+}
+
+func main() {
+	// Let flag parse the command line arguments and then input the first argument in to the repo variable.
+	flag.Parse()
+	repo := flag.Arg(0)
+
+	// Handle empty arguments by printing usage for out program.
+	if repo == "" {
+		log.Fatalln("Usage: solfetch [repository name]")
+	}
+
+	// Run the git command to download the whole repository from upstream.
+	fetch(Sprintf("git clone https://dev.getsol.us/source/%s.git", repo)
+}
