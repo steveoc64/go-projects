aacf56fbed83cfe6e2935ccd4be2c52a0fcdb082
Jacob <Jacalz@users.noreply.github.com>
2019-10-15 19:44:48 +0200

    Make our concurrent solbuild be faster and scale threads better

--- projects/solus-packaging/solfetch/conurrent-solbuild.go
+++ projects/solus-packaging/solfetch/conurrent-solbuild.go
@@ -21,66 +21,52 @@ func exists(path string) bool {
 }
 
 // The fetch command runs the git clone command and prints the output to terminal.
-func fetch(command string, result chan []byte) {
+func fetch(command string, result chan string) {
 	fetch := exec.Command("sh", "-c", command)
 	output, err := fetch.CombinedOutput()
 	if err != nil && command != "" {
 		panic(err)
 	}
 
-	result <- output
+	result <- string(output)
 }
 
 func main() {
 	// Parse three first arguments in to repo slice.
 	flag.Parse()
-	repo := [3]string{flag.Arg(0), flag.Arg(1), flag.Arg(2)}
+	repo := flag.Args()
 
 	// Don't proceed if they alread are fetched.
-	if exists(repo[0]) || exists(repo[1]) || exists(repo[2]) {
-		log.Fatalln("Don't fetch repos that are already fetched!")
+	for i := range repo {
+		if exists(repo[i]) {
+			log.Fatalln("Don't fetch repos that are already fetched!")
+		}
 	}
 
-  // Handle empty arguments and tell user how to use program.
-  if repo[0] == "" {
-    log.Fatalln("Usage: solfetch [repository name] [optional] [optional]")
-  }
+	// Handle empty arguments and tell user how to use program.
+	if repo[0] == "" {
+		log.Fatalln("Usage: solfetch [repository name] [optional] [optional]")
+	}
 
 	// Start up the three channels for communication.
-	var first, second, third chan []byte
-
-  if repo[0] != "" {
-    first = make(chan []byte)
-  }
-  if repo[1] != "" {
-    second = make(chan []byte)
-  }
-  if repo[2] != "" {
-    third = make(chan []byte)
-  }
+	var chanel []chan string
+	for range repo {
+		chanel = append(chanel, make(chan string))
+	}
 
 	// Start up cocurrent taskt for fetching up to three repos at once.
-  if repo[0] != "" {
-		go fetch(fmt.Sprintf("git clone https://dev.getsol.us/source/%s.git", repo[0]), first)
-  }
-  if repo[1] != "" {
-		go fetch(fmt.Sprintf("git clone https://dev.getsol.us/source/%s.git", repo[1]), second)
-  }
-  if repo[2] != "" {
-		go fetch(fmt.Sprintf("git clone https://dev.getsol.us/source/%s.git", repo[2]), third)
-  }
+	for i := range repo {
+		go fetch(fmt.Sprintf("git clone https://dev.getsol.us/source/%s.git", repo[i]), chanel[i])
+	}
 
 	// Fetch the outputs from each fetch run.
-  var one, two, three []byte
-
-  if repo[0] != "" && repo[1] == "" && repo[2] == "" {
-    one = <-first
-  } else if repo[0] != "" && repo[1] != "" && repo[2] == "" {
-    one, two = <-first, <-second
-  } else if repo[0] != "" && repo[1] != "" && repo[2] != "" {
-    one, two, three = <-first, <-second, <-third
-  }
+	var outputs []string
+	for i := range repo {
+		outputs = append(outputs, <-chanel[i])
+	}
 
 	// Print output to terminal:
-	fmt.Printf("%s%s%s", one, two, three)
+	for i := range outputs {
+		fmt.Print(outputs[i])
+	}
 }
