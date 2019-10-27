f3c5eacba157215dc796b2ecdbce53e1da298e1c
Jacob <Jacalz@users.noreply.github.com>
2019-10-27 09:04:42 +0100

    Basically a fully functionalluffarschack game
    Just need to make my own fancy icons and maybe add an icon to the start button.

--- school/luffarschack/luffarschack.go
+++ school/luffarschack/luffarschack.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"time"
+	"fmt"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 )
 
 // Global variable for defining if a button is pressed or not.
-var clicked = [9]bool{}
+var clicked = [9]int8{}
+
+// Global variables for which parts the players own.
+var player1 = [9]bool{false, false, false, false, false, false, false, false, false}
+var player2 = [9]bool{false, false, false, false, false, false, false, false, false}
+
+// Our global index for defining if it's player one or player two's turn to play.
+var index int8
 
 // InitGUI starts up the whole interface for out program.
-func InitGUI(channel chan bool) {
+func InitGUI() {
 	// Initialize our new fyne interface application.
 	app := app.New()
 
@@ -29,31 +37,31 @@ func InitGUI(channel chan bool) {
 	// Define all variables we need for the buttons.
 	var (
 		button1 = widget.NewButton("", func() {
-			clicked[0] = true
+			clicked[0] = 1
 		})
 		button2 = widget.NewButton("", func() {
-			clicked[1] = true
+			clicked[1] = 1
 		})
 		button3 = widget.NewButton("", func() {
-			clicked[2] = true
+			clicked[2] = 1
 		})
 		button4 = widget.NewButton("", func() {
-			clicked[3] = true
+			clicked[3] = 1
 		})
 		button5 = widget.NewButton("", func() {
-			clicked[4] = true
+			clicked[4] = 1
 		})
 		button6 = widget.NewButton("", func() {
-			clicked[5] = true
+			clicked[5] = 1
 		})
 		button7 = widget.NewButton("", func() {
-			clicked[6] = true
+			clicked[6] = 1
 		})
 		button8 = widget.NewButton("", func() {
-			clicked[7] = true
+			clicked[7] = 1
 		})
 		button9 = widget.NewButton("", func() {
-			clicked[8] = true
+			clicked[8] = 1
 		})
 	)
 
@@ -61,10 +69,10 @@ func InitGUI(channel chan bool) {
 	vbox.Append(widget.NewButton("Click to start", func() {
 		// Make sure to clear all earlier button presses before proceeding.
 		for i := range clicked {
-			clicked[i] = false
+			clicked[i] = 0
 		}
 
-		// Also remove all our icons before proceeding.
+		// Also remove all our icons before proceeding if we are restarting.
 		button1.SetIcon(nil)
 		button2.SetIcon(nil)
 		button3.SetIcon(nil)
@@ -75,66 +83,129 @@ func InitGUI(channel chan bool) {
 		button8.SetIcon(nil)
 		button9.SetIcon(nil)
 
-		for index := 0; index < 9; index++ {
+		// Clear index and player owned tiles.
+		index = 0
+		player1 = [9]bool{false, false, false, false, false, false, false, false, false}
+		player2 = [9]bool{false, false, false, false, false, false, false, false, false}
 
-			// Sleep in order to wait for inputs from the user.
-			time.Sleep(time.Duration(5 * time.Second))
+		// Define the variable for telling that we won.
+		won := false
+
+		for won == false {
 
 			// Ugly switch statement to set the icons for our buttons.
-			switch {
-			case clicked[0]:
+			if clicked[0] == 1 {
+				clicked[0]++
 				if index%2 == 0 {
 					button1.SetIcon(circle)
+					player1[0] = true
 				} else {
 					button1.SetIcon(cross)
+					player2[0] = true
 				}
-			case clicked[1]:
+				index++
+			} else if clicked[1] == 1 {
+				clicked[1]++
 				if index%2 == 0 {
 					button2.SetIcon(circle)
+					player1[1] = true
 				} else {
 					button2.SetIcon(cross)
+					player2[1] = true
 				}
-			case clicked[2]:
+				index++
+			} else if clicked[2] == 1 {
+				clicked[2]++
 				if index%2 == 0 {
 					button3.SetIcon(circle)
+					player1[2] = true
 				} else {
 					button3.SetIcon(cross)
+					player2[2] = true
 				}
-			case clicked[3]:
+				index++
+			} else if clicked[3] == 1 {
+				clicked[3]++
 				if index%2 == 0 {
 					button4.SetIcon(circle)
+					player1[3] = true
 				} else {
 					button4.SetIcon(cross)
+					player2[3] = true
 				}
-			case clicked[4]:
+				index++
+			} else if clicked[4] == 1 {
+				clicked[4]++
 				if index%2 == 0 {
 					button5.SetIcon(circle)
+					player1[4] = true
 				} else {
 					button5.SetIcon(cross)
+					player2[4] = true
 				}
-			case clicked[5]:
+				index++
+			} else if clicked[5] == 1 {
+				clicked[5]++
 				if index%2 == 0 {
 					button6.SetIcon(circle)
+					player1[5] = true
 				} else {
 					button6.SetIcon(cross)
+					player2[5] = true
 				}
-			case clicked[6]:
+				index++
+			} else if clicked[6] == 1 {
+				clicked[6]++
 				if index%2 == 0 {
 					button7.SetIcon(circle)
+					player1[6] = true
 				} else {
 					button7.SetIcon(cross)
+					player2[6] = true
 				}
-			case clicked[7]:
+				index++
+			} else if clicked[7] == 1 {
+				clicked[7]++
 				if index%2 == 0 {
 					button8.SetIcon(circle)
+					player1[7] = true
 				} else {
 					button8.SetIcon(cross)
+					player2[7] = true
 				}
-			case clicked[8]:
+				index++
+			} else if clicked[8] == 1 {
+				clicked[8]++
 				if index%2 == 0 {
 					button9.SetIcon(circle)
+					player1[8] = true
 				} else {
 					button9.SetIcon(cross)
+					player2[8] = true
+				}
+				index++
+			}
+
+			fmt.Println("Player 1:", player1, "Player 2:", player2)
+
+			// Check if our index is bigger than 9, because then we are finished. If the index is bigger or equal to 4, we can check for a win.
+			if index > 9 {
+				break
+			} else if index >= 4 {
+				if CheckWon(player1) {
+					// Show a dialogue informing the first player that he won!
+					message := dialog.NewInformation("Player 1 has won!", "Congratulations to player 1 for winning.", window)
+					message.Show()
+					break
+				} else if CheckWon(player2) {
+					// Show a dialogue informing the second player that he won!
+					message := dialog.NewInformation("Player 2 has won!", "Congratulations to player 2 for winning.", window)
+					message.Show()
+					break
+				} else if index == 9 {
+					message := dialog.NewInformation("It is a tie!", "Nobody has won. Please try better next time.", window)
+					message.Show()
+					break
 				}
 			}
 
@@ -150,18 +221,39 @@ func InitGUI(channel chan bool) {
 	window.SetContent(vbox)
 
 	// Set the size to something small but good looking and usable.
-	window.Resize(fyne.NewSize(350, 100))
+	window.Resize(fyne.NewSize(400, 100))
 
 	// Show all of our set content and run the gui.
 	window.ShowAndRun()
-
-	channel <- true
 }
 
 func main() {
-	channel := make(chan bool)
+	InitGUI()
+}
 
-	go InitGUI(channel)
+// CheckWon checks all possible combinations for winning.
+func CheckWon(player [9]bool) bool {
 
-	<-channel
+	// All possible combinations for winning.
+	if player[0] && player[1] && player[2] {
+		return true
+	} else if player[0] && player[3] && player[6] {
+		return true
+	} else if player[0] && player[3] && player[5] {
+		return true
+	} else if player[6] && player[7] && player[8] {
+		return true
+	} else if player[2] && player[5] && player[8] {
+		return true
+	} else if player[0] && player[4] && player[8] {
+		return true
+	} else if player[2] && player[4] && player[6] {
+		return true
+	} else if player[1] && player[4] && player[7] {
+		return true
+	} else if player[3] && player[4] && player[5] {
+		return true
+	}
+
+	return false
 }
